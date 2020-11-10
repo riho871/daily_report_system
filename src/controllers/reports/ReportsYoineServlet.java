e4269cff8027a91b15d843683dc1c8478f06a66d
riho.shibuichi <riho871@gmail.com>
2020-11-10 14:43:35 +0900

    Add flush message

--- src/controllers/reports/ReportsYoineServlet.java
+++ src/controllers/reports/ReportsYoineServlet.java
@@ -1,12 +1,19 @@
 package controllers.reports;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Report;
+import models.validators.ReportValidator;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class ReportsYoineServlet
@@ -27,4 +34,33 @@ public class ReportsYoineServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }}
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+            
+            Report r = em.find(Report.class, (Integer)(request.getSession().getAttribute("report_id")));
+
+            r.setYoine(Integer.valueOf(request.getParameter("yoine")));
+            
+            List<String> errors = ReportValidator.validate(r);
+            if(errors.size() > 0) {
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("report", r);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
+                rd.forward(request, response);
+            } else {
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "いいねしました。");
+                em.close();
+                
+                response.sendRedirect(request.getContextPath() + "/reports/index");
+            }
+        }
+    }
+
+}
